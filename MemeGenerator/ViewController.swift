e01ccf048d24ad9d71ab3f63422056797e1df503
AlperAkinci <alperakinci1@gmail.com>
2015-12-10 13:58:47 +0200

    Picking an Image finished
    -camera
    -camera roll

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -8,16 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+   
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+    }
+        
+    
+    
+      
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
+        
+        let pickerController = UIImagePickerController()
+       
+        pickerController.delegate = self
+    
+        pickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        self.presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
+   
+    
+    @IBAction func pickAnImageFromCamera(sender: AnyObject) {
+        
+        let pickerController = UIImagePickerController()
+        
+        pickerController.delegate = self
+        
+        
+        
+        pickerController.sourceType = UIImagePickerControllerSourceType.Camera
+        
+        self.presentViewController(pickerController, animated: true, completion: nil)
+    }
+
+    
+    
+    
+    
+    
+    //get access to an image chosen from the Photo library or camera
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        
+        
+        imagePickerView.image = image
+        
+        //imagePickerView.contentMode = UIViewContentMode.ScaleToFill
+        
+        
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
 
