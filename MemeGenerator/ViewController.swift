7da01b5e60ea9d21bccc2d0e4388d4e2de7a6f1d
AlperAkinci <alperakinci1@gmail.com>
2015-12-12 14:28:17 +0200

    Moving a view out of the way of keyboard , Done
    NSNotification used
    
    Problem :
    it takes double size of keyboard thats why i divide by 2 in upper
    functions (keyboardWillHide , keyboardWillShow)

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavi
    
    
     // Textfield Delegate Objects
+    
     var toptextfield = TextfieldDelegate()
     var bottomtextfield = TextfieldDelegate()
 
@@ -39,15 +40,26 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavi
     
     
     override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        subscribeToKeyboardNotifications()
+        
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
-        
-    
-    
-    
     
 
     
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    
+    
+    
+    //MARK:Image Picking Stuff
+
     @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
         
         let pickerController = UIImagePickerController()
@@ -67,32 +79,61 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavi
         
         pickerController.delegate = self
         
-        
-        
         pickerController.sourceType = UIImagePickerControllerSourceType.Camera
         
         self.presentViewController(pickerController, animated: true, completion: nil)
     }
 
     
-    
-    
-    
-    
     //get access to an image chosen from the Photo library or camera
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
-        
         
         
         imagePickerView.image = image
         
         //imagePickerView.contentMode = UIViewContentMode.ScaleToFill
-        
-        
-        
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
+    
+   //MARK:NSNotification stuff
+    
+   //Sign up to be notified when the keyboard appears
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:"    , name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:"    , name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    //Sign up to be notified when the keyboard disappears
+    func unsubscribeFromKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:
+            UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:
+            UIKeyboardWillHideNotification, object: nil)
+
+    }
+    
+   //When the keyboardWillShow notification is received, shift the view's frame up
+    func keyboardWillShow(notification: NSNotification) {
+        view.frame.origin.y -= getKeyboardHeight(notification)/2
+    }
+    
+    //When the keyboardWillHide notification is received, shift the view's frame down
+    func keyboardWillHide(notification: NSNotification) {
+        view.frame.origin.y += getKeyboardHeight(notification)/2
+    }
+    
+    
+    // Problem : it takes double size of keyboard thats why
+    // i divide by 2 in upper functions
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
+    }
 
 }
 
